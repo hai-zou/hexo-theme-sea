a0fb930950eca7e299ac7782a4d572ea23f07965
zouhai <zou.hai@outlook.com>
2024-01-19 10:49:18 +0800

    feat: 调整移动端导航栏

--- themes/sea/source/js/mobile_nav.js
+++ themes/sea/source/js/mobile_nav.js
@@ -3,6 +3,7 @@
 
     var body = document.body;
     var navToggle = document.getElementById('sea-mobile-nav-toggle');
+    var dimmer = document.getElementById('sea-mobile-nav-dimmer');
     var CLASS_NAME = 'sea-mobile-nav-on';
     if (!navToggle) return;
 
@@ -10,5 +11,12 @@
         e.preventDefault();
         e.stopPropagation();
         body.classList.toggle(CLASS_NAME);
+    });
+
+    dimmer.addEventListener('click', function(e) {
+        if (!body.classList.contains(CLASS_NAME)) return;
+    
+        e.preventDefault();
+        body.classList.remove(CLASS_NAME);
     });
 }());
