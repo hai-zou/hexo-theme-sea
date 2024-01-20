3b6ef7688c7df7631d658916e430fc23bee52d32
zouhai <zou.hai@outlook.com>
2024-01-20 21:58:19 +0800

    chore: 删除cheerio库

--- themes/sea/scripts/helpers.js
+++ themes/sea/scripts/helpers.js
@@ -1,16 +1,1 @@
 'use strict';
-
-const cheerio = require('cheerio');
-
-hexo.extend.helper.register('page_anchor', str => {
-    const $ = cheerio.load(str, { decodeEntities: false });
-    const headings = $('h1, h2, h3, h4, h5, h6');
-
-    if (!headings.length) return str;
-
-    headings.each(function () {
-        $(this).addClass('article-heading');
-    });
-
-    return $.html();
-});
