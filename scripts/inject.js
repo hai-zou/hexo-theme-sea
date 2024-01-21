0d872f647c52c21fca47e9b44e41b0a150f9d8b5
zouhai <zou.hai@outlook.com>
2024-01-21 18:29:26 +0800

    feat: 谷歌分析

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,3 +1,7 @@
 hexo.extend.injector.register('head_end', '<meta name="baidu-site-verification" content="codeva-JdN4jbMZRk">');
 hexo.extend.injector.register('head_end', '<meta name="google-site-verification" content="djOHUZEvT4rsmXBBvUZ5S0MjQ_7nRdwc49_D5_6uhPk">');
 hexo.extend.injector.register('head_end', '<meta name="keywords" content="前端博客,个人技术博客,前端,前端开发,前端框架,web前端,学习,JavaScript,js,ES6,TypeScript,vue,css3,html5,Node,git,github,markdown">');
+
+// 谷歌分析
+hexo.extend.injector.register('head_end', '<script async src="https://www.googletagmanager.com/gtag/js?id=G-WW5WXCXDFX"></script>');
+hexo.extend.injector.register('head_end', "<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-WW5WXCXDFX');</script>");
