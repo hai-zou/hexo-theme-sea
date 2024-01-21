d8c3f93df45f319fd6eb33e1876b3ea72e3feac1
zouhai <zou.hai@outlook.com>
2024-01-21 17:28:13 +0800

    feat: 网站关键词

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,2 +1,3 @@
 hexo.extend.injector.register('head_end', '<meta name="baidu-site-verification" content="codeva-JdN4jbMZRk">');
 hexo.extend.injector.register('head_end', '<meta name="google-site-verification" content="djOHUZEvT4rsmXBBvUZ5S0MjQ_7nRdwc49_D5_6uhPk">');
+hexo.extend.injector.register('head_end', '<meta name="keywords" content="前端博客,个人技术博客,前端,前端开发,前端框架,web前端,学习,JavaScript,js,ES6,TypeScript,vue,css3,html5,Node,git,github,markdown">');
