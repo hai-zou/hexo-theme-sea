48fb533c5594775157951b5a82db847c29b9ebf9
zouhai <zou.hai@outlook.com>
2025-01-24 11:14:31 +0800

    refactor: 首页改版

---
+++ scripts/filters.js
@@ -0,0 +1,9 @@
+hexo.extend.filter.register('template_locals', locals => {
+  const allPosts = locals.site.posts.sort('-date').toArray(); // 按日期降序排序
+  const recommendedPosts = allPosts.filter(post => post.recommend === true); // 筛选推荐文章
+
+  // 将结果保存到 locals 中，供模板使用
+  locals.recommendedPosts = recommendedPosts.slice(0, 5);
+  locals.recentPosts = allPosts.slice(0, 5);
+  return locals;
+});
