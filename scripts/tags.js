be89c856c716259bd30a7ed083ad9fc17445f6f6
zouhai <zou.hai@outlook.com>
2025-04-05 17:19:52 +0800

    feat: 新增作品卡片

--- scripts/tags.js
+++ scripts/tags.js
@@ -34,3 +34,37 @@ hexo.extend.tag.register('friends', () => {
   const friendsEleStr = friendsEle.join('');
   return `<div class="sea-friend-wrapper">${friendsEleStr}</div>`;
 });
+
+hexo.extend.tag.register('works', () => {
+  const works = hexo.theme.config.works || [];
+  const getTags = (tags) => {
+    if (!tags) return '';
+    return tags.map(item => `<span class="sea-works-tag">${item}</span>`).join('');
+  };
+  const worksEle = works.map(item => {
+    return `
+      <div class="sea-works-card">
+        ${item.cover ?`<a
+          class="sea-works-cover"
+          href="${item.link}"
+          target="_blank"
+        >
+          <img src="${item.cover}" alt="${item.name}" />
+        </a>` : ''}
+        <div class="sea-works-content">
+          <a
+            class="sea-works-title"
+            href="${item.link}"
+            target="_blank"
+            title="${item.name}"
+          >
+            ${item.name}
+          </a>
+          <div class="sea-works-desc" title="${item.desc}">${item.desc}</div>
+          <div class="sea-works-tags">${getTags(item.tags)}</div>
+        </div>
+      </div>
+    `;
+  });
+  return `<div class="sea-works-wrapper">${worksEle.join('')}</div>`;
+});
