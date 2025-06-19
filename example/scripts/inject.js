aa26ba78373b2b4fba1c9ec2153c523c6f38e30d
zouhai <zou.hai@outlook.com>
2025-06-19 17:20:14 +0800

    feat: use LXGW font

---
+++ example/scripts/inject.js
@@ -0,0 +1,3 @@
+// 插入自定义样式
+hexo.extend.injector.register('head_end', '<link rel="stylesheet" href="https://cdn.bootcdn.net/ajax/libs/lxgw-wenkai-screen-webfont/1.7.0/style.min.css" />');
+hexo.extend.injector.register('head_end', '<style>:root{--sea-font-family:"LXGW WenKai Screen", sans-serif;}</style>');
